d64dd91d8e63f525a9d070f069fc15ffc47c5c51
RDeveLabs <rdevelopamd@gmail.com>
2026-06-10 14:13:16 +0700

    update konfigurasi database

--- connection.js
+++ connection.js
@@ -2,11 +2,10 @@ const mysql = require("mysql2/promise");
 require('dotenv').config(); 
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "rdevelabs",
-  database: process.env.DB_NAME || "villa",
-  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
